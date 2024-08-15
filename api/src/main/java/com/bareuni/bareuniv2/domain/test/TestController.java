fd3939ac938998ede240a32e330acc967c3e8cf4
seheonnn <ho78901@naver.com>
2024-08-15 20:11:53 +0900

    test: orphanRemoval 테스트 코드 작성

---
+++ api/src/main/java/com/bareuni/bareuniv2/domain/test/TestController.java
@@ -0,0 +1,30 @@
+package com.bareuni.bareuniv2.domain.test;
+
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bareuni.coredomain.domain.user.User;
+import com.bareuni.coredomain.domain.user.repository.UserRepository;
+import com.bareuni.coredomain.global.ApiResponse;
+import com.bareuni.coreinfrasecurity.annotation.UserResolver;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RequiredArgsConstructor
+@Transactional
+// @RequestMapping("/api/v2/auth")
+@RestController
+public class TestController {
+
+	private final UserRepository userRepository;
+
+	@GetMapping("/test-user")
+	public ApiResponse<User> testUserProfile(@UserResolver User user) {
+		user.setUserImage(null);
+		userRepository.save(user);
+		return ApiResponse.onSuccess(user);
+	}
+}
---
+++ api/src/test/java/com/bareuni/bareuniv2/domain/user/service/UserServiceTest.java
@@ -0,0 +1,70 @@
+package com.bareuni.bareuniv2.domain.user.service;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.bareuni.coredomain.domain.user.GenderType;
+import com.bareuni.coredomain.domain.user.RoleType;
+import com.bareuni.coredomain.domain.user.User;
+import com.bareuni.coredomain.domain.user.UserImage;
+import com.bareuni.coredomain.domain.user.repository.UserImageRepository;
+import com.bareuni.coredomain.domain.user.repository.UserRepository;
+
+@SpringBootTest
+@Transactional
+class UserServiceTest {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@Autowired
+	private UserImageRepository userImageRepository;
+
+	@BeforeEach
+	void setUp() {
+
+		User user = userRepository.save(
+			User.builder()
+				.email("test@example.com")
+				.username("testuser")
+				.password("password")
+				.gender(GenderType.MALE)
+				.age(30)
+				.role(RoleType.USER)
+				.build()
+		);
+
+		UserImage userImage = userImageRepository.save(
+			UserImage.builder()
+				.user(user)
+				.url("http://example.com/image.jpg")
+				.build()
+		);
+
+		// User와 UserImage 간의 연관 관계 설정
+		user.setUserImage(userImage);
+		userRepository.save(user);
+	}
+
+	@Test
+	void testUserImageOrphanRemoval() {
+
+		// given
+		User findUser = userRepository.findById(1L).get();
+
+		// when
+		findUser.setUserImage(null);
+		userRepository.save(findUser);
+
+		// then
+		Optional<UserImage> userImage = userImageRepository.findById(1L);
+		Assertions.assertTrue(userImage.isEmpty());
+	}
+
+}
